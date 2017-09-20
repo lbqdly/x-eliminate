4eb9d57ec6d92c108dd59d0c0a0c40afd9f2e1ca
good318 <304469323@qq.com>
2017-09-20 11:10:00 +0800

    up

--- webpack.config.js
+++ webpack.config.js
@@ -37,7 +37,7 @@ module.exports = {
     entry: './index.js',
     plugins: [
         new HtmlWebpackPlugin({
-            filename: 'xe.html',
+            filename: 'index.html',
             template: 'index.ejs'
         }),
         //静态文件包，直接copy到发布目录。
@@ -50,7 +50,7 @@ module.exports = {
             new BrowserSyncPlugin({
                     server: {
                         baseDir: "dist",
-                        index: "xe.html"
+                        index: "index.html"
                     }
                 },
                 {reload: true}
